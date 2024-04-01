bb1609570f89ff6e1b30e7e540c9e1dd0fa68a83
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 <enpassant.rack@gmail.com>
2024-04-01 19:29:28 +0300

    Iterations-5

--- scraper.js
+++ scraper.js
@@ -55,7 +55,7 @@ app.post('/scrape', async (req, res) => {
     const products = await page.evaluate(() => {
       const productList = [];
 
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < 5; i++) {
         const priceEl = document.querySelectorAll('.b-list-advert__price-base .qa-advert-price')[i];
         if (!priceEl) break;
 
